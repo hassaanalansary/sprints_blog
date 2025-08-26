35b63f4fb36c7e260b9b822aa0621deb801749ee
hassaanalansary <hassaanalansary@yahoo.com>
2025-08-26 19:01:13 +0300

    revert changes in settings.py

--- sprints_blog/settings.py
+++ sprints_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-xd+u11na_4ymd%vu^)%k7em*e7534slp&*ti0m9m-5lgx*0lmu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
