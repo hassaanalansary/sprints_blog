81b5f121754bd9915452d0a269bf03e8f7130286
hassaanalansary <hassaanalansary@yahoo.com>
2025-10-10 18:09:41 +0300

    create user when form is valid

--- users/views.py
+++ users/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from users.models import User
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["name", "email", "username", "password1", "password2"]
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            email = form.cleaned_data.get("email")
+            name = form.cleaned_data.get("name")
+            user = User(username=username, name=name, email=email)
+            user.set_password(password)
+            user.save()
+            return redirect("blog-home")
+    form = UserRegistrationForm()
     return render(request, 'users/register.html', context={'form': form})
