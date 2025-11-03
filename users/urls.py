885f35aa5fe9c089629ab04fa5293ffe3f0fd3a8
hassaanalansary <hassaanalansary@yahoo.com>
2025-11-04 01:30:05 +0200

    Add edit profile page

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import register, profile
+from .views import register, profile, edit_profile
 from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path("register/", register, name= 'register'),
     path("profile/", profile, name= 'profile'),
+    path("profile/update/", edit_profile, name= 'edit-profile'),
     path("login/", LoginView.as_view(template_name="users/login.html"), name= 'login'),
     path("logout/", LogoutView.as_view(template_name="users/logout.html"), name= 'logout'),
 
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from users.models import User
 from django.contrib import messages
+from django import forms
 class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
@@ -28,3 +29,25 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["name", "email", "username", "profile_image"]
+        widgets = {
+           "name": forms.TextInput(attrs= {"class": "validate"}),
+           "email": forms.EmailInput(attrs= {"class": "validate"}),
+           "username": forms.TextInput(attrs= {"class": "validate"}),
+        }
+
+
+def edit_profile(request):
+    if request.method == "POST":
+        form = UserUpdateForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully.")
+            return redirect('profile')
+    else:
+        form  = UserUpdateForm(instance=request.user)
+    return render(request, "users/edit_profile.html", {'form':form})
